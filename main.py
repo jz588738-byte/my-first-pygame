84e95caa91cde44669c6d5695b2c98ffd74bd707
jz588738-byte <jz588738@gmail.com>
2026-01-23 22:57:35 +0800

    增加了碰撞偵測和子彈，也有優化射擊手感

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ pygame.display.set_caption('雷霆戰機')
 WHITE = (255,255,255)
 GREEN = (0,255,0)
 RAD = (255,0,0)
+YELLOW = (255,255,0)
+BLACK = (0,0,0)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -25,18 +27,33 @@ class Player(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH // 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 8
+        #自動射擊
+        self.shoot_delay = 250
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
+        #鍵盤控制邏輯
         key_pressed = pygame.key.get_pressed()
         if key_pressed[pygame.K_d]:
             self.rect.x += self.speedx
         if key_pressed[pygame.K_a]:
             self.rect.x -= self.speedx
-
+        #自動射擊
+        if key_pressed[pygame.K_SPACE]:
+            self.shoot()
+        #邊界控制
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+
+    def shoot(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+            self.last_shot = now
+            bullet = Bullet(self.rect.centerx,self.rect.top)
+            all_sprites.add(bullet)
+            bullets.add(bullet)
 
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
@@ -57,12 +74,30 @@ class Rock(pygame.sprite.Sprite):
             self.speedy = random.randrange(2, 5)
             self.speedx = random.randrange(2, 5)
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.Surface((10,20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
 all_sprites = pygame.sprite.Group()
+rocks = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
 for _ in range(10):
     rock = Rock()
     all_sprites.add(rock)
+    rocks.add(rock)
 
 #遊戲迴圈
 running = True
@@ -74,8 +109,18 @@ while running:
             running = False
     #更新遊戲
     all_sprites.update()
+    #石頭和子彈的碰撞
+    hits = pygame.sprite.groupcollide(rocks,bullets,True,True)
+    for hit in hits:
+        rock = Rock()
+        all_sprites.add(rock)
+        rocks.add(rock)
+    #飛機和石頭的碰撞
+    hits = pygame.sprite.spritecollide(player,rocks,False)
+    if hits:
+        running = False
     #畫面顯示
-    screen.fill(WHITE)
+    screen.fill(BLACK)
     all_sprites.draw(screen)
     pygame.display.update()
 
