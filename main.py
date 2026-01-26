ff710d23d9d1737f8e8d5c757036b7ea6e9393a7
jz588738-byte <jz588738@gmail.com>
2026-01-26 23:23:07 +0800

    'feat: 增加分數系統'

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ rocks_img = []
 for i in range(7):
     rocks_img.append(pygame.image.load(os.path.join('image',f'rock{i}.png')).convert())
 
+#載入字體
+font_name = pygame.font.match_font('arial')
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.centerx = x
+    text_rect.top = y
+    surf.blit(text_surface, text_rect)
+
 #創立各物件的群組，方便做碰撞判斷
 all_sprites = pygame.sprite.Group()
 rocks = pygame.sprite.Group()
@@ -32,6 +42,8 @@ for _ in range(10):
     all_sprites.add(r)
     rocks.add(r)
 
+score = 0
+
 #遊戲迴圈
 running = True
 while running:
@@ -40,27 +52,32 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    #更新遊戲
+
     #自動射擊
     key_pressed = pygame.key.get_pressed()
     if key_pressed[pygame.K_SPACE]:
         player.shoot(all_sprites, bullets, bullet_img)
-
+    # 更新全部的物件
     all_sprites.update()
     #石頭和子彈的碰撞
     hits = pygame.sprite.groupcollide(rocks,bullets,True,True)
+
     for hit in hits:
+        score += int(hit.radius)
         r = Rock(rocks_img)
         all_sprites.add(r)
         rocks.add(r)
+
     #飛機和石頭的碰撞
     hits = pygame.sprite.spritecollide(player,rocks,False,pygame.sprite.collide_circle)
     if hits:
         running = False
+
     #畫面顯示
     screen.fill(BLACK)
     screen.blit(background, (0,0))
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH // 2 , 0)
     pygame.display.update()
 
 pygame.quit()
