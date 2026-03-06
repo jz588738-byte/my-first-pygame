cbacfcb05b11b617808ef22ed91bb53befdbff52
jz588738-byte <jz588738@gmail.com>
2026-03-06 23:43:44 +0800

    style: 修正 ui_utils.py 的縮排與代碼排版

--- ui_utils.py
+++ ui_utils.py
@@ -3,8 +3,9 @@ import pygame
 from resource_manager import BASE_DIR
 from setting import *
 
-#載入字體
+# 載入字體
 font_name = os.path.join(BASE_DIR, 'font', 'font.ttf')
+
 def Draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
     text_surface = font.render(text, True, WHITE)
@@ -13,7 +14,7 @@ def Draw_text(surf, text, size, x, y):
     text_rect.top = y
     surf.blit(text_surface, text_rect)
 
-#顯示生命值
+# 顯示生命值
 def Draw_health(surf, hp, x, y):
     if hp < 0:
         hp = 0
@@ -32,22 +33,22 @@ def Draw_lives(surf, lives, img, x, y):
         img_rect.y = y
         surf.blit(img, img_rect)
 
-#初始畫面
+# 初始畫面
 def Draw_init(surf, res):
     clock = pygame.time.Clock()
-
     surf.blit(res['img']['background'], (0, 0))
     Draw_text(surf, '太空生存戰!!!',64 ,WIDTH / 2, HEIGHT / 4)
     Draw_text(surf, 'AD鍵控制左右移動，空白鍵可以射擊。',23 , WIDTH / 2, HEIGHT / 2)
     Draw_text(surf, '點任意鍵可以開始遊戲。',23 , WIDTH / 2, (HEIGHT / 3) * 2)
     pygame.display.update()
-    #初始化的迴圈
+    
     waiting = True
     while waiting:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                return
             elif event.type == pygame.KEYUP:
                 waiting = False
 
@@ -58,7 +59,7 @@ def Draw_end_screen(surf, res, score):
     Draw_text(surf, '按下 [R] 重新開始', 23, WIDTH / 2, (HEIGHT / 3) * 2)
     Draw_text(surf, '按下 [M] 回到主選單', 23, WIDTH / 2, (HEIGHT / 3) * 2 + 40)
     pygame.display.update()
-    #結算畫面的迴圈
+    
     waiting = True
     while waiting:
         pygame.time.Clock().tick(FPS)
