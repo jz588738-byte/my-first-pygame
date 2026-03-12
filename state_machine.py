375e08f9d5a09d28b484cd8396a72b8474e893b8
jz588738-byte <jz588738@gmail.com>
2026-03-12 22:15:12 +0800

    perf: 增加了字典來存取state，而不是在切換狀態時，再生成狀態，用以提升效率

--- state_machine.py
+++ state_machine.py
@@ -9,7 +9,7 @@ class State:
     def exit(self):
         pass
 
-    def update(self):
+    def update(self, events = None):
         pass
 
     def draw(self, screen):
@@ -17,21 +17,30 @@ class State:
 
 #處理狀態轉變的過程
 class StateMachine:
-    def __init__(self):
+    def __init__(self, owner=None):
+        self.owner = owner
         self.current_state = None
+        self.states = {}
 
-    def change_state(self, new_state_instance):
+    def add_state(self, state_name, state_instance):
+        self.states[state_name] = state_instance
+
+    def change_state(self, state_name):
+        if state_name not in self.states:
+            print(f"Warning: State '{state_name}' not found in StateMachine.")
+            return
+
         if self.current_state:
             self.current_state.exit()
         
-        self.current_state = new_state_instance
+        self.current_state = self.states[state_name]
         
         if self.current_state:
             self.current_state.enter()
 
-    def update(self):
+    def update(self, events = None):
         if self.current_state:
-            self.current_state.update()
+            self.current_state.update(events)
 
     def draw(self, screen):
         if self.current_state:
