b662ea3f9736900732d09f19a07a2cdafe3878f5
jz588738-byte <jz588738@gmail.com>
2026-03-15 17:30:51 +0800

    feat: 加入的rusher物件，並在sprites的__init__中增加導出rusher
    1.rusher一樣用狀態機轉換、確保狀態的物件導向
    
    2.在生成rusher，加入了create_rusher，用了區間分割法解決了單次生成， 可能會有飛機重疊的情況
    
    3.在particle中新增create_fire的這一個生成方法，可以讓讓粒子朝某一個扇形區域飛行
    
    加入了rusher的圖片

--- sprites/__init__.py
+++ sprites/__init__.py
@@ -4,6 +4,6 @@ from .bullet import Bullet
 from .explosion import Explosion
 from .power_up import Power_up
 from .rocks import BaseRock, SplitRock, ExplodingRock
-from .enemies import BaseEnemy, Sniper
+from .enemies import BaseEnemy, Sniper, Rusher
 from .particle import Particle
 from .laser import Laser
---
+++ sprites/enemies/rusher/rusher_states.py
@@ -0,0 +1,66 @@
+from setting import HEIGHT
+import pygame
+from state_machine import State
+from setting import *
+import random
+from ...particle import Particle
+
+class RusherBaseState(State):
+    def __init__(self, owner):
+        super().__init__(owner)
+        self.game = owner.game
+        self.res = owner.res
+
+class EntryState(RusherBaseState):
+    def enter(self):
+        self.owner.state_label = 'Entry'
+
+    def update(self, events=None):
+        self.owner.rect.y += self.owner.speed_y
+        if self.owner.rect.top >= 0:
+            self.owner.rect.top = 0
+            self.owner.state_machine.change_state('WarningState')
+
+class WarningState(RusherBaseState):
+    def enter(self):
+        self.owner.state_label = 'Warning'
+        self.start_time = pygame.time.get_ticks()
+
+    def update(self, events=None):
+        now = pygame.time.get_ticks()
+        if now - self.start_time >= 1000:
+            self.owner.state_machine.change_state('BurstState')
+
+    def draw(self, screen):
+        end_pos = (self.owner.rect.centerx, HEIGHT)
+        pygame.draw.line(screen, (165, 95, 25), self.owner.rect.center, end_pos, 2)
+
+
+class BurstState(RusherBaseState):
+    def enter(self):
+        self.owner.state_label = 'Burst'
+        self.start_time = pygame.time.get_ticks()
+        self.owner.image = self.game.res['img']['rusher_go']
+    
+    def update(self, events=None):
+        now = pygame.time.get_ticks()
+        #查看玩家是否有經過rusher的方向
+        if abs(self.owner.rect.centerx - self.game.player.rect.centerx) < 25:
+            self.owner.has_boosted = True
+            self.owner.image = self.game.res['img']['rusher_burst']
+            
+        
+        if now - self.start_time >= 1000:
+            if self.owner.has_boosted:
+                self.owner.rect.y += self.owner.burst_speed_y
+            else:
+                self.owner.rect.y += self.owner.speed_y
+        
+        if self.owner.rect.top >= HEIGHT:
+            self.owner.kill()
+    
+    def draw(self, screen):
+        if self.owner.has_boosted:
+            Particle.create_fire(self.game, self.owner.rect, (105, 5, 140))
+        else:
+            Particle.create_fire(self.game, self.owner.rect, (10, 25, 130))
